1125443658fff69c0739d7b2cbbe43447aed8fe2
mainbosskay <dadakehinde80@yahoo.com>
2024-07-26 02:47:10 +0000

    Implementing HolbertonCorse class with name, length and students verified

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,48 @@
+class HolbertonCourse {
+  constructor(name, length, students) {
+  this.name = name;
+  this.length = length;
+  this.students = students;
+  }
+
+  // Gets and sets name for the course 
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  // Gets and sets length for the course
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typrof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  // Gets and sets students taking the course
+  get students() {
+    return this._students;
+  }
+
+  set tudents(newStudents) {
+    if (!(newStudents instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    if (!newStudents.every((studentName) => typeof studentName === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = newStudents;
+  }
+}
+
+export default HolbertonCourse;
