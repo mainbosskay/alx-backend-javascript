4f2e09c9a171a82f14366693a41a18b46df066a8
mainbosskay <dadakehinde80@yahoo.com>
2024-07-26 04:45:28 +0000

    Implementing Airport with name and code, return airport code

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,30 @@
+class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  // Getting and setting nameof Airport
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  // Getting and setting code of the Airport
+  get code() {
+    return this._coode;
+  }
+
+  set code(newCode) {
+  this._code = newCode;
+  }
+
+  // Getting default str description for Airport and return code
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
+export default Airport;
