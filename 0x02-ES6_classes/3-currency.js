7cd3c2b7326320a142d0cf32e14e38f0c6b158a4
mainbosskay <dadakehinde80@yahoo.com>
2024-07-26 03:36:29 +0000

    Implementing Currency with code and name

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,29 @@
+class Currency {
+  contructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  // Gets and sets code of the currency
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  // Gets and setsname of the currency
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
+export default Currency;
