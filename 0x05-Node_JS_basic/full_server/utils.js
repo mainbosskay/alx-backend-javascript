8301d6698b6efa4bf3fd3e2f3d478a7606a100b7
mainbosskay <dadakehinde80@yahoo.com>
2024-09-18 20:27:19 +0000

    Creating  full server in a directory named full_server

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -10,15 +10,15 @@ function readDatabase(dbPath) {
       const dbContent = data.toString().split('\n');
       const studentRecords = dbContent.filter((item) => item);
       const students = studentRecords.map((item) => item.split(','));
-    
+
       const studentsField = {};
       for (const indx in students) {
         if (indx !== 0) {
-	  if (!studentsField[students[indx][3]]) studentsField[students[indx][3]] = [];
+          if (!studentsField[students[indx][3]]) studentsField[students[indx][3]] = [];
           studentsField[students[indx][3]].push(students[indx][0]);
-	}
+        }
       }
-    
+
       delete studentsField.field;
       resolve(studentsField);
     });
