c24b083d6c2ba1c3ae5a33de4a2d27c5cec4dc84
mainbosskay <dadakehinde80@yahoo.com>
2024-09-18 13:28:30 +0000

    Creating small HTTP server using the http module and it should be assigned to the variable app

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,12 +4,12 @@ const port = 1245;
 const host = 'localhost';
 const app = http.createServer();
 
-app.on('request', (_, resp) => {
+app.on('request', (_, res) => {
   const respMssge = 'Hello Holberton School!';
-  resp.setHeader('Content-Type', 'text/plain');
-  resp.setHeader('Content-Length', respMssge.length);
-  resp.statusCode = 200;
-  resp.write(Buffer.from(respMssge));
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', respMssge.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(respMssge));
 });
 
 app.listen(port, host, () => {
