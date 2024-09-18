500b259862b3497fd9bf81b2f427b9f400623d4e
mainbosskay <dadakehinde80@yahoo.com>
2024-09-18 12:14:36 +0000

    Using database database.csv createcountStudents in the file 2-read_file.js

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function countStudents(dbPath) {
+  let dbContent;
+  try {
+    dbContent = fs.readFileSync(dbPath);
+  } catch (FileError) {
+    throw new Error ('Cannot load the database');
+  }
+
+  const lines = dbContent.toString().split('\n');
+  const studentRecords = lines.filter((item) => item);
+  const students = studentRecords.map((item) => item.split(','));
+  const totalStudents = students.length ? students.length -1 : 0;
+  console.log(`Number of students: ${totalStudents}`);
+
+  const studentsField = {};
+  for (const indx in students) {
+    if (indx !== 0) {
+      if (!studentsField[students[indx][3]]) studentsField[students[indx][3]] = [];
+      studentsField[students[indx][3]].push(students[indx][0]);
+    }
+  }
+
+  delete studentsField.field;
+
+  for (const field of Object.keys(studentsField)) {
+    console.log(
+      `Number of students in ${field}: ${studentsField[field].length}. List: ${studentsField[field].join(', ')}`
+    );
+  }
+}
+
+module.exports = countStudents;
