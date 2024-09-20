6f6f2efeddeb98989d89a83d8aaefd51a2a318ee
mainbosskay <dadakehinde80@yahoo.com>
2024-09-20 10:26:35 +0000

    Rewrite tests to Chai for BDD style and update test file

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,19 @@
+const calculateNumber = (type, a, b) => {
+  const firstNum = Math.round(a);
+  const scndNum = Math.round(b);
+
+  if (type === 'SUM') {
+    return firstNum + scndNum;
+  }
+
+  if (type === 'SUBTRACT') {
+    return firstNum - scndNum;
+  }
+
+  if (type === 'DIVIDE') {
+    return scndNum === 0 ? 'Error' : firstNum / scndNum;
+  }
+  return 0;
+};
+
+module.exports = calculateNumber;
